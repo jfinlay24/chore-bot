e5b42eeddc1ba0f5819e14b37d38fab8977a9562
Jonathan Finlay <jfinlay24@yahoo.ie>
2020-08-10 18:30:54 +0100

    Random generator on door working

--- script.js
+++ script.js
@@ -6,12 +6,38 @@ const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-
 const beachDoorPath ="https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
 const spaceDoorPath ="https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
 
+const numClosedDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+const randomChoreDoorGenerator = () => {
+  let choreDoor = Math.floor(Math.random() * numClosedDoors);
+  if(choreDoor === 0) {
+    openDoor1 = botDoorPath;
+    openDoor2 = beachDoorPath;
+    openDoor3 = spaceDoorPath;
+  } 
+  else if(choreDoor === 1) {
+    openDoor2 = botDoorPath;
+    openDoor1 = spaceDoorPath;
+    openDoor3 = beachDoorPath;
+  } 
+  else {
+    openDoor3 = botDoorPath;
+    openDoor2 = spaceDoorPath;
+    openDoor1 = beachDoorPath;
+  }
+}
+
 door1.onclick = () => {
-  doorImage1.src = botDoorPath;
+  doorImage1.src = openDoor1;
 }
 door2.onclick = () => {
-  doorImage2.src = beachDoorPath;
+  doorImage2.src = openDoor2;
 }
 door3.onclick = () => {
-  doorImage3.src = spaceDoorPath;
+  doorImage3.src = openDoor3;
 }
+
+randomChoreDoorGenerator();
