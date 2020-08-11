c2067a67c503e2e22ce03d1f08760c2fdf839e46
Jonathan Finlay <jfinlay24@yahoo.ie>
2020-08-11 21:45:07 +0100

    win rules set

--- script.js
+++ script.js
@@ -2,15 +2,33 @@
 let doorImage1 = document.getElementById('door1');
 let doorImage2 = document.getElementById('door2');
 let doorImage3 = document.getElementById('door3');
+let startButton = document.getElementById('start');
 const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
 const beachDoorPath ="https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
 const spaceDoorPath ="https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+const closedDoorPath ="https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg";
 
-const numClosedDoors = 3;
+let numClosedDoors = 3;
 let openDoor1;
 let openDoor2;
 let openDoor3;
 
+const isClicked = (door) => {
+  if(door.src === closedDoorPath) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+const playDoor = () => {
+  numClosedDoors--;
+  alert(numClosedDoors);
+  if(numClosedDoors === 0) {
+    gameOver('win');
+  }  
+}
 const randomChoreDoorGenerator = () => {
   let choreDoor = Math.floor(Math.random() * numClosedDoors);
   if(choreDoor === 0) {
@@ -31,13 +49,28 @@ const randomChoreDoorGenerator = () => {
 }
 
 door1.onclick = () => {
-  doorImage1.src = openDoor1;
+  if(!isClicked(doorImage1)) {
+    doorImage1.src = openDoor1;
+    playDoor();
+  }
 }
 door2.onclick = () => {
-  doorImage2.src = openDoor2;
+  if(!isClicked(doorImage2)) {
+    doorImage2.src = openDoor2;
+    playDoor();
+  }
 }
 door3.onclick = () => {
-  doorImage3.src = openDoor3;
+  if(!isClicked(doorImage3)) {
+    doorImage3.src = openDoor3;
+    playDoor();
+  }
+}
+
+const gameOver = (status) => {
+  if(status === 'win') {
+    startButton.innerHTML = "You win! Play again?";
+  }
 }
 
 randomChoreDoorGenerator();
